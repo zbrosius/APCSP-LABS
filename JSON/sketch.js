5c6f34221bce2cb19621e83ffacc5eb6f6143d5e
zbrosius <42615348+zbrosius@users.noreply.github.com>
2018-11-06 08:47:08 -0800

    Add files via upload

---
+++ JSON/sketch.js
@@ -0,0 +1,17 @@
+var data;
+
+function preload(){
+data = loadJSON("population.json");
+console.log(data);
+}
+
+function setup(){
+  data.countrydata[3].males
+
+}
+
+function bubbleSort(){
+  var temp = 0;
+  var males = countrydata.males
+  for(var i = countrydata.male.length -1; i >)
+}
