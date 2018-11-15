eabb05740acae3f2b20107d31121ae72d339f304
zbrosius <42615348+zbrosius@users.noreply.github.com>
2018-11-14 22:35:09 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -23,3 +23,52 @@ function bubbleSort(myarray){
             }
         }
     }
+  
+  
+  
+  
+  var w = 20;
+  var cols, rows;
+  var snake;
+  
+  functionsetup() 
+  var cnv = createCanvas(800, 800);
+  cols = width/w;
+  rows = height/w;
+  cnv.position((windowWidth-width)/2, 30);
+  frameRate(3);
+  background(20,20,20);
+  snake = new Snake(createVector(width/2,height/2), createVector(1, 0));
+}
+function draw() {
+  snake.run();
+}
+
+function keyPressed(){
+  text(keyCode, 50,50);
+  if(keyCode === UP_ARROW){
+    snake.vel = createVector(0,-1*w)
+  }
+}
+
+
+
+
+fuction Snake(loc, vel)(
+  
+  this.update();
+  this.render();
+}
+
+this.update = function(){
+  this.vel.mult(w);
+  this.loc.add(this.vel);
+  this.loc.x = constrain(this.loc.x, 0, width-w);
+  this.loc.y = constrain(this.loc.y, 0, height-w);
+}
+
+this.render = function(){
+  fill(0,255,0);
+  rect(this.loc.x, this.loc.y, w, w)
+}
+}
